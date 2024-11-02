3fa74dd7df49c17a37ba3168ae25195c2a5b7ca3
Fairyghost007 <biranenini6762@gmail.com>
2024-11-02 10:46:09 +0000

    age implementation

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Card.css'; // For additional custom styling
 
-const Card = ({ name, team, nationality, jerseyNumber, imageUrl, stats }) => {
+const Card = ({ name, team, nationality, jerseyNumber, imageUrl, stats, age }) => {
   return (
     <div className="fifa-card text-white border-4 relative m-2 rounded-2xl bg-yellow-400">
       {/* Card Header */}
         <div className="flex flex-col gap-1 items-center absolute    lg:mt-24 md:mt-20  mt-12 ml-10 lg:ml-6 md:ml-8 sm:ml-4">
           <h3 className="text-yellow-400 lg:text-3xl md:text-2xl sm:text-xl font-bold">{jerseyNumber}</h3>
-          <span className="lg:text-xl md:text-lg sm:text-sm text-yellow-400 font-bold">LW</span>
+          <span className="lg:text-xl md:text-lg sm:text-sm text-yellow-400 font-bold">{age}</span>
           <img src={team} alt={`${name} team logo`} className="w-4 h-4 lg:w-10 md:w-5 sm:w-3 lg:h-10 md:h-5 sm:h-3  " />
           <img src={nationality} alt={`${name} nationality`} className=" w-4 h-4 lg:w-10 md:w-5 sm:w-3 lg:h-10 md:h-5 sm:h-3  " />
         </div>
@@ -41,6 +41,7 @@ Card.defaultProps = {
   nationality: "/images/argentina.png",
   jerseyNumber: 99,
   imageUrl: "https://via.placeholder.com/150",
+  age: 99,
   stats: {
     pace: 98,
     shooting: 99,
